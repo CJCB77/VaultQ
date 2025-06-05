c17a05fcc25f0e966a4574e302910d9edf780b90
Johnny Coral <coral.johnny.2@gmail.com>
2025-06-05 17:43:46 -0500

    Created custom user, custom command and refactored docker-compose file

---
+++ backend/app/user/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractUser, # type: ignore
+    BaseUserManager, # type: ignore
+)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField('email address', unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+    
