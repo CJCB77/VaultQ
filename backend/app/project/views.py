31d7bf12e1a460c2f7fe7bbc0f2d8fe2d737f3f1
Johnny Coral <coral.johnny.2@gmail.com>
2025-06-07 17:49:09 -0500

    Created viewsets for my project model

--- backend/app/project/views.py
+++ backend/app/project/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+"""
+API Views for the Project model
+"""
 
-# Create your views here.
+from models import Project
+from serializers import (
+    ProjectListSerializer,
+    ProjectDetailSerializer,
+)
+
+from rest_framework import (
+    viewsets,
+    mixins,
+    status
+)
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    """View for managing project API"""
+    queryset = Project.objects.all().order_by('-created_at')
+    serializer_class = ProjectDetailSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def get_queryset(self):
+        """Retrieve projects for the authenticated user"""
+        return self.queryset.filter(user=self.request.user)
+    
+    def get_serializer_class(self):
+        """Return the serializer class for the request"""
+        if self.action == "list":
+            return ProjectListSerializer
+
+        return self.serializer_class
