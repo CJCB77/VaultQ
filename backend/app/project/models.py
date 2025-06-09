40aaebdc40407a86f8c3f8ff560f45fc7ac7106e
Johnny Coral <coral.johnny.2@gmail.com>
2025-06-08 22:19:20 -0500

    Added document model

--- backend/app/project/models.py
+++ backend/app/project/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.conf import settings
 
 
+def document_upload_path(instance, filename):
+    """Generate upload path for documents"""
+    return f'projects/{instance.project.id}/documents/{filename}'
+
+
 class Project(models.Model):
     """Project model for organizing documents and vector stores"""
     name = models.CharField(
@@ -35,3 +40,52 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Document(models.Model):
+    """Document model for handling project related files"""
+
+    class ProcessingStatus(models.TextChoices):
+        PENDING = 'pending', 'Pending'
+        PROCESSING = 'processing', 'Processing',
+        COMPLETED = 'completed', 'Completed'
+        FAILED = 'failed', 'Failed'
+
+    name = models.CharField(
+        max_length=255,
+        help_text="Original File Name"
+    )
+    project = models.ForeignKey(
+        'Project', 
+        on_delete=models.CASCADE,
+        related_name="documents",
+        help_text="Project this document belongs"
+    )
+    file_path = models.FileField(
+        upload_to=document_upload_path,
+        help_text="Uploaded document file"
+    )
+    file_size = models.PositiveIntegerField(
+        help_text="File size in bytes"
+    )
+    content_type = models.CharField(
+        max_length=100,
+        help_text="MIME type of the file"
+    )
+    processing_status = models.CharField(
+        max_length=200,
+        choices=ProcessingStatus.choices,
+        default=ProcessingStatus.PENDING,
+        help_text="Current processing status"
+    )
+    chunks_count = models.PositiveIntegerField(
+        help_text="Number of chunks created from this document"
+    )
+    uploaded_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="uploaded_documents",
+        help_text="User who uploaded the document"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- backend/app/project/tests/test_document_api.py
+++ backend/app/project/tests/test_document_api.py
@@ -3,15 +3,23 @@ Tests for the Document model API
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-from ..models import Project
+from project.models import Project
 import tempfile
-import os
 
 from rest_framework.test import APIClient
 from rest_framework import status
 
+from ..models import Project 
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from ..serializers import DocumentSerializer
+
 User = get_user_model()
+
+def get_project_documents_url(project_id):
+    return reverse('project:project-documents', args=[project_id])
 
 class DocumentModelTest(TestCase):
     """Test cases for Document model"""
@@ -23,8 +31,59 @@ class DocumentModelTest(TestCase):
             password='pass12345'
         ) # type: ignore
         self.client.force_authenticate(self.user)
-
         self.project = Project.objects.create(
             name="Test Project",
             description="Test project description"
         )
+    
+    def test_upload_document_to_project(self):
+        sample_pdf_file = SimpleUploadedFile(
+            name='test.pdf',
+            content='Test content',
+            content_type='application/pdf'
+        )
+        URL = get_project_documents_url(self.project.id)
+        res = self.client.post(URL, sample_pdf_file)
+
+        project_document = Document.objects.get(id=res.id)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(project_document.name, 'test.pdf')
+    
+    def test_uploaded_files_fit_content_type(self):
+        sample_image_file = SimpleUploadedFile(
+            name='test_image.jpeg',
+            content=b'some_binary_content',
+            content_type='image/jpeg).'
+        )
+        URL = get_project_documents_url(self.project.id)
+        res = self.client.post(URL, sample_image_file)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertFalse(Document.objects.filter(id=res.id).exists())
+
+    def test_documents_limited_to_project(self):
+          Document.objects.create(
+            name="test.pdf"
+            file = 'projects/project1/documents/test.pdf'
+            content_type = 'application/pdf'
+            project = self.project
+        )
+        Document.objects.create(
+            name="test2.pdf"
+            file = 'projects/project1/documents/test2.pdf'
+            content_type = 'application/pdf'
+            project = self.project
+        )
+
+        URL = get_project_documents_url(self.project.id)
+        res = self.client.get(URL)
+        documents = Document.objects.filter(project=self.project)
+        serializer = DocumentSerializer(documents)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_donwload_project_document(self):
+        pass
+
